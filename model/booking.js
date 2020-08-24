b06168ef9750a96da4d5296edf4ae435f4d1f533
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-08-24 11:18:52 +0200

    Updated booking model

--- model/booking.js
+++ model/booking.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let BookingSchema = new Schema({
-    id: Number,
+    // id: Number,
     date: String,
     time: String,
     quantity: Number,
     message: String,
-    customerId: Number,
+    customerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Customer"
+    }
 });
 
 const BS = mongoose.model('Booking', BookingSchema);
