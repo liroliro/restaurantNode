20eda17374f2cdc1bb1af22f526f5f769e9075a8
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-08-24 12:20:37 +0200

    Removes dead code

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -7,40 +7,6 @@ const router = express.Router();
 const BookingModel = require('../model/booking')
 const CustomerModel = require('../model/customer')
 
-// router.post('/', async (req, res) => {
-//     const user = await CustomerModel.findOne({ email: req.body.email });
-
-//     if(!user) {
-//       await new CustomerModel({
-//             firstName: string = req.body.firstName,
-//             lastName: string = req.body.lastName,
-//             email: string = req.body.email,
-//             phone: string = req.body.phone,
-//         }).save()
-//     } else {
-//         user.save();
-//     }
-
-
-//     console.log('customer posted')
-// }); 
-
-
-// router.post('/booking', async(req,res)=> {
-//     const booking = await CustomerModel.findOne({ id: req.body._id });
-
-//     await new BookingModel({
-//         date: number = req.body.date,
-//         time: number = req.body.time,
-//         quantity: number = req.body.quantity,
-//         message: string = req.body.message,
-//         customerId: string = booking._id
-//     }).save()
-
-//     console.log('booking posted')
-// })
-
-
 
 router.post('/', async (req, res) => {
   const user = await CustomerModel.findOne({ email: req.body.email });
