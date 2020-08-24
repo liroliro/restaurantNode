be6cf5e0da1fce614664b3a5e744cb2a7653c577
Oscar Mattsson <oscarmatttsson@gmail.com>
2020-08-24 12:17:22 +0200

    Updated post to include both customer & booking-details

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const config = require("../config/config");
 
 // const nodeMailer = require("nodemailer");
 // const sendGridTransport = require("nodemailer-sendgrid-transport");
@@ -8,43 +7,72 @@ const config = require("../config/config");
 const BookingModel = require('../model/booking')
 const CustomerModel = require('../model/customer')
 
+// router.post('/', async (req, res) => {
+//     const user = await CustomerModel.findOne({ email: req.body.email });
 
-router.get('/', async(req,res) => {
-    res.render('index')
-});
+//     if(!user) {
+//       await new CustomerModel({
+//             firstName: string = req.body.firstName,
+//             lastName: string = req.body.lastName,
+//             email: string = req.body.email,
+//             phone: string = req.body.phone,
+//         }).save()
+//     } else {
+//         user.save();
+//     }
 
-router.get('/booking', async(req,res) => {
-    res.render('booking')
-});
+
+//     console.log('customer posted')
+// }); 
+
+
+// router.post('/booking', async(req,res)=> {
+//     const booking = await CustomerModel.findOne({ id: req.body._id });
+
+//     await new BookingModel({
+//         date: number = req.body.date,
+//         time: number = req.body.time,
+//         quantity: number = req.body.quantity,
+//         message: string = req.body.message,
+//         customerId: string = booking._id
+//     }).save()
+
+//     console.log('booking posted')
+// })
+
+
 
 router.post('/', async (req, res) => {
-    const user = await CustomerModel.findOne({ email: req.body.email });
+  const user = await CustomerModel.findOne({ email: req.body.email });
 
-    await new CustomerModel({
-        firstName: string = req.body.firstName,
-        lastName: string = req.body.lastName,
-        email: string = req.body.email,
-        phone: string = req.body.phone,
-    }).save()
+  if (!user) {
+    new CustomerModel({
+      firstName: (string = req.body.firstName),
+      lastName: (string = req.body.lastName),
+      email: (string = req.body.email),
+      phone: (string = req.body.phone),
+    })
+      .save()
+      .then(async () => {
+        const newUser = await CustomerModel.findOne({ email: req.body.email });
 
-    console.log('customer posted')
-}); 
-
-
-router.post('/booking', async(req,res)=> {
-    const booking = await CustomerModel.findOne({ id: req.body._id });
-
-    await new BookingModel({
-        date: number = req.body.date,
-        time: number = req.body.time,
-        quantity: number = req.body.quantity,
-        message: string = req.body.message,
-        customerId: string = booking._id
-    }).save()
-
-    console.log('booking posted')
-})
-
-
+        new BookingModel({
+          date: (number = req.body.date),
+          time: (number = req.body.time),
+          quantity: (number = req.body.quantity),
+          message: (string = req.body.message),
+          customerId: (string = newUser._id),
+        }).save();
+      });
+  } else {
+    new BookingModel({
+      date: (number = req.body.date),
+      time: (number = req.body.time),
+      quantity: (number = req.body.quantity),
+      message: (string = req.body.message),
+      customerId: (string = user._id),
+    }).save();
+  }
+});
 
 module.exports = router; 
