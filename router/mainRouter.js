fb18f0ef97c77d5fa83155989e863df8db7fb5c1
Caroline Svanemar <caroline.svanemar@hotmail.com>
2020-09-07 12:13:58 +0200

    added nodemailer

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
-// const nodeMailer = require("nodemailer");
-// const sendGridTransport = require("nodemailer-sendgrid-transport");
-
+const nodemailer = require('nodemailer');
+const config = require('../config/config');
 const BookingModel = require('../model/booking');
 const CustomerModel = require('../model/customer');
+
+
+let smtpConfig = {
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true, // use SSL
+
+	auth: {
+		user: config.email,
+		pass: config.password,
+	}
+};
+
+let transporter = nodemailer.createTransport(smtpConfig);
+
+
+
 
 router.get('/bookings', async (req, res) => {
 	const bookings = await BookingModel.find();
 
 	res.send(bookings);
 });
+
 
 router.get('/customers', async (req, res) => {
 	const customers = await CustomerModel.find();
@@ -20,6 +36,7 @@ router.get('/customers', async (req, res) => {
 
 router.post('/', async (req, res) => {
 	const user = await CustomerModel.findOne({ email: req.body.email });
+	console.log(user)
 
 	if (!user) {
 		new CustomerModel({
@@ -38,6 +55,7 @@ router.post('/', async (req, res) => {
 					guests: (number = req.body.guests),
 					message: (string = req.body.message),
 					customerId: (string = newUser._id),
+
 				}).save();
 			});
 	} else {
@@ -48,22 +66,72 @@ router.post('/', async (req, res) => {
 			message: (string = req.body.message),
 			customerId: (string = user._id),
 		}).save();
+		console.log(b)
 	}
+
+	let mailContent = {
+		from: 'restaurantdinnerspace@gmail.com',
+		to: user.email,
+		subject: `Välkommen till DinnerSpace ${user.firstName}`,
+		text: `Välkommen till Dinner Space ${user.firstName + " " + user.lastName}. 
+		Du har bokat bord hos oss den ${user.date} klockan ${user.time}.00.
+		Vi hoppas ni får en trevlig middag hos oss. Skulle ni behöva göra ändringar i er bokning
+		är ni välkomna att ringa oss på telefonnummer 08-123 345`
+	};
+
+	transporter.sendMail(mailContent, function (error, info) {
+
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent (info.response): ', info.response);
+		}
+
+	});
+	console.log(user);
+	res.send({
+		success: true,
+	});
 });
 
 router.delete('/delete/:id', async (req, res) => {
-	await BookingModel.deleteOne({ _id: req.params.id });
+
+	const booking = await BookingModel.findOne({ _id: req.params.id });
+	const guest = await CustomerModel.findOne({
+		_id: booking.customerId
+
+	});
+
+	let mailDeleteContent = {
+		from: 'restaurantdinnerspace@gmail.com',
+		to: guest.email,
+		subject: `Din avbokning hos DinnerSpace ${guest.firstName}.`,
+		text: `Hej ${guest.firstName + " " + guest.lastName}.
+	Här kommer en bekräftelse på din avbokning för ${ booking.date} klockan ${booking.time} .00.
+	Vi hoppas vi ses en annan gång.
+		Hälsningar
+	gänget på DinnerSpace`
+	};
+
+	transporter.sendMail(mailDeleteContent, function (error, info) {
+
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent (info.response): ', info.response);
+			/* 	console.log(guest); */
+			console.log(booking);
+		}
+	});
+	booking.delete();
+	res.send();
+
 });
+
+
 
 router.put('/update/:id', async (req, res) => {
 	await BookingModel.updateOne({ _id: req.params.id });
 });
-
-// Gammalt
-// router.get('/deleteWishlist/:id', verifyToken, async (req, res) => {
-// 	const user = await User.findOne({ _id: req.user.user._id });
-// 	await user.removeFromList(req.params.id);
-// 	res.redirect('/wishlist');
-// });
 
 module.exports = router;
